af754480b6cb7dfa23743d8b3c993f27c34d68ae
Philip Dubois <heazky@gmail.com>
2016-11-22 17:35:57 +0100

    Fix typo

--- manifests/snapshot.pp
+++ manifests/snapshot.pp
@@ -52,8 +52,9 @@ class elasticsearch::snapshot(
   } elsif ($type == 's3') {
     if ($base_path) {
       $settings = "{\"bucket\": \"${bucket}\",\"region\": \"${region}\",\"base_path\": \"${base_path}\"}"
-    } else
+    } else {
       $settings = "{\"bucket\": \"${bucket}\",\"region\": \"${region}\"}"
+    }
     }
   }
 
